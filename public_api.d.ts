44c22ab09364e5d3e87fa70a30884a3e9554ebc2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-13 14:33:48 +0000

    Deploy emazv72/ngx-imageviewer to github.com/emazv72/ngx-imageviewer.git:gh-pages

--- public_api.d.ts
+++
@@ -1,3 +0,0 @@
-export { ImageViewerModule } from './lib/imageviewer.module';
-export { ImageViewerConfig, createButtonConfig, IMAGEVIEWER_CONFIG } from './lib/imageviewer.config';
-export { ImageViewerComponent } from './lib/imageviewer.component';
